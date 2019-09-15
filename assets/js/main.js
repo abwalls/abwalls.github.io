1fb30d6eb12f61a9ad75086f5d403eda0d3e52d1
abwalls <andrew-walls@live.com>
2019-09-15 13:43:45 -0400

    Added Overlays

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,19 +4,22 @@ window.onorientationchange = function() { document.body.scrollTop = 0; }
 
 
 document.getElementById("about-icon").addEventListener("click", function(){
-    document.querySelector("#about").style.display = "block"
+    document.querySelector("#about").style.display = "block";
+    document.getElementById("overlay").style.backgroundColor = "rgba(0,0,0,.3)";
 })
 
 document.getElementById("about-header-close").addEventListener("click", function(){
-    document.querySelector("#about").style.display = "none"
+    document.querySelector("#about").style.display = "none";
+    document.getElementById("overlay").style.backgroundColor = "rgba(0,0,0,0)";
 })
 
 
 document.getElementById("contact-icon").addEventListener("click", function(){
-    document.querySelector("#contact").style.display = "block"
-    document.querySelector("#about").style.display = "none"
+    document.querySelector("#contact").style.display = "block";
+    document.querySelector("#about").style.display = "none";
+    document.getElementById("overlay").style.backgroundColor = "rgba(255,255,255,.3)";
 })
 
 document.getElementById("about-icon").addEventListener("click", function(){
-    document.querySelector("#contact").style.display = "none"
+    document.querySelector("#contact").style.display = "none";
 })
